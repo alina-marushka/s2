a03b34069aa5b4f9146f0e4253f80f4356b42b08
alina-marushka <101595575+alina-marushka@users.noreply.github.com>
2022-05-06 21:38:48 +0300

    Initialize andrew_poliakov_homework_java

---
+++ aTemplate/src/main/java/by/jrr/start/Application.java
@@ -0,0 +1,13 @@
+package by.jrr.start;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World and java guru!");
+        fewCode();
+    }
+
+    public static void fewCode(){
+        System.out.println("a few lines of code");
+    }
+}
